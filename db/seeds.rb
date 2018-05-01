f7754f4cd1008c707fb8ad1cd4f9e4334ea5b84f
mainRomainRo <romain.fiorotto@gmail.com>
2018-05-01 12:48:21 +0200

    site terminé

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+movies = Movie.create!([{title: 'Whiplash', release_year: '2014', director: 'Damien Chazelle'}, 
+	{title: 'Dangal', release_year: '2016', director: 'Nitesh Tiwari'},
+	{title: 'Alice au pays des merveilles', release_year: '2014', director: 'Tim Burton'},
+	{title: 'Pulp Fiction', release_year: '2006', director: 'Tarantino'},
+	{title: 'Scarface', release_year: '1998', director: 'Scorcese'
+		}])	
